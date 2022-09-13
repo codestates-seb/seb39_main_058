e0ff04a7acb68b66b6d9a7ae01b3af9489701df7
Hansjade <Hanssstt@gmail.com>
2022-09-13 13:58:35 +0900

    initial

---
+++ server/sswitch/sswitch/src/main/java/main/sswitch/SswitchApplication.java
@@ -0,0 +1,13 @@
+package main.sswitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SswitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SswitchApplication.class, args);
+	}
+
+}
