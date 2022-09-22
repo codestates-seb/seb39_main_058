fc319ac9fb03d889be8f88e3dc8cabab3d360d2f
Jakin-Kim <kimyagin94@gmail.com>
2022-09-22 11:03:58 +0900

    feat/community_create: 자유게시판 작성 페이지 css 완성

--- client/src/pages/Community/CommunityCreate.js
+++ client/src/pages/Community/CommunityCreate.js
@@ -6,34 +6,31 @@ function CommunityCreate() {
   return (
     <Main>
         <div className='container'>
-            <div className='board-header'>
+            <BoardHeader>
                 <h1>자유게시판</h1>
                 <h2>자유롭게 의견을 게시해주세요.</h2>
                 <div className='warning'>
                     ※ 욕설이나 비방성 글은 경고없이 삭제될 수 있습니다.
                 </div>
-            </div>
+            </BoardHeader>
             
             <BoardWrite>
                 <div className='writer-title'>
                     <span>제목</span>
                     <input type='text' placeholder='제목을 입력해주세요.'/>
                 </div>
-                {/* <div className='writer-info'>
-                    <div className='writer-id'>
-                        <span>작성자ID</span>
-                        <input type='text' placeholder='아이디를 입력해주세요.'/>
-                    </div>
-                    <div className='writer-pw'>
-                        <span>비밀번호</span>
-                        <input type='password' placeholder='비밀번호를 입력해주세요.'/>
-                    </div>
-                </div> */}
+                
                 <div className='writer-content'>
-                    <textarea placeholder='내용을 입력해주세요.'></textarea>
+                    <textarea placeholder='내용을 입력해주세요.'/>
                 </div>
+                <div className='writer-tags'></div>
             </BoardWrite>
+
             {/* 등록 및 취소하는 버튼 만들기 */}
+            <ButtonWrapper>
+                <button className='writer-submit'> 등록 </button>
+                <button className='writer-cancel'> 취소 </button>
+            </ButtonWrapper>
         </div>
 
     </Main>
@@ -43,59 +40,126 @@ function CommunityCreate() {
 export default CommunityCreate;
 
 const Main = styled.main`
-    
-    border: 1px solid black; // 삭제 예정
+    /* border: 1px solid black; // 삭제 예정 */
     width: 100vw;
-    height: 90vh;
+    height: 92vh;
+    background-color: ivory;
 
     .container {
         display: flex;
         flex-direction: column;
         align-items: center;
-        margin: 100px;
-
-        .board-header {
-            width: 90vw;
-            border-bottom: 3px solid black;
-        }
-
-        h1 {
-            font-size: 4vmin;
-            font-family: 'Courier New', Courier, monospace;
-            letter-spacing: 3px;
-        }
-        
-        h2{
-            font-size: 2vmin;
-            margin-bottom: 0;
-            font-family: 'Courier New', Courier, monospace;
-        }
-        .warning {
-            color: red;
-            font-size: 1.5vmin;
-            padding-bottom: 10px;
-        }
     }
+`;
 
+const BoardHeader = styled.div`
+
+    width: 90vw;
+    border-bottom: 3px solid black;
+
+    h1 {
+        font-size: 4vmin;
+        font-family: 'Courier New', Courier, monospace;
+        letter-spacing: 3px;
+    }
+    
+    h2{
+        font-size: 2vmin;
+        margin-bottom: 0;
+        font-family: 'Courier New', Courier, monospace;
+    }
+    .warning {
+        color: red;
+        font-size: 1.5vmin;
+        padding-bottom: 1vh;
+    }
 `;
 
 const BoardWrite = styled.div`
-
-    display: flex;
-    flex-direction: column;
-    * {
-        padding: 10px 20px;
-    }
+    
+    *{
+        margin: 1rem;
+    } 
 
     .writer-title {
+        /* width: 85vw; */
+        padding: 10px 20px;
+        border-bottom: 2px solid lightgray;
 
-    }
+        span {
+            margin: 20px;
+            font-size: 2vmin;
+            font-family: 'Courier New', Courier, monospace;
+        }
 
-    .writer-info {
-        display: flex;
+        input {
+            width: 60vw;
+            height: 4vh;
+            padding: 0 15px;
+            font-size: 2vmin;
+            background-color: ivory;
+        }
     }
 
     .writer-content {
+        textarea{
+            width: 85vw;
+            height: 30vh;
+            padding: 10px 15px;
+            font-size: 2vmin;
+            background-color: ivory;
+            resize: none;
+        }
+    }
+`;
 
+const ButtonWrapper = styled.div`
+
+    @media (max-width: 500px) {
+        display: flex;
+        flex-direction: column;
+
+        .writer-submit {
+            width: 50vw;
+            margin: 0px;
+        }
+
+        .writer-cancel {
+            width: 50vw;
+            margin: 0px;
+        }
+    }
+
+    display: flex;
+
+    .writer-submit {
+        margin: 1rem;
+        padding: 1vmin 4vmin;
+        border-radius: 1rem;
+        border: 1px solid gray;
+        background-color: #38d9a9;
+        color: white;
+        font-size: 1.5vmin;
+        cursor: pointer;
+        &:hover {
+            color: white;
+            background-color: rgb(71,182,181);
+            border: 1px solid rgb(71,182,181);
+        }
+    }
+
+    .writer-cancel {
+        margin: 1rem;
+        padding: 1vmin 4vmin;
+        border-radius: 1rem;
+        border: 1px solid gray;
+        background-color: ivory;
+        font-size: 1.5vmin;
+        cursor: pointer;
+        &:hover {
+            color: black;
+            background-color: lightgray;
+            border: 1px solid lightgray;
+        }
     }
 `;
