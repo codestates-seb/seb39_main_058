20d1217145c81fc636bedb851821deccaa0d91ec
Jakin-Kim <kimyagin94@gmail.com>
2022-09-26 16:28:29 +0900

    feat/community_detail: comment 데이터를 CommunityAnswer 컴포넌트에 props로 전달

--- client/src/pages/Community/CommunityDetail.js
+++ client/src/pages/Community/CommunityDetail.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import CommunityAnswer from '../../components/CommunityAnswer/CommunityAnswer'
 
 // 특정 질문을 눌렀을 때 나오는 세부 페이지
 function CommunityDetail() {
+  const { id } = useParams();
+
+  const [ comment, setComment ] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://ec2-43-200-66-53.ap-northeast-2.compute.amazonaws.com:8080/community/forum/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.data.commentResponses)
+        setComment(data.data.commentResponses)
+      })
+      .catch(err => console.log(err))
+  })
+
+  
+
   return (
       <>
         <div>CommunityDetail</div>
         
-        <CommunityAnswer/>
+        <CommunityAnswer comment={comment} setComment={setComment}/>
       </>
   )
 }
