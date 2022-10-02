0a91002af2c509f388b8454886813670aa5fdd8b
seokminkk <99874863+seokminkk@users.noreply.github.com>
2022-10-02 15:18:35 +0900

    feat/login: 구글oauth 라이브러리안쓰고 정보만 받아오는거까지 구현 백엔드랑 같이 마무리해야 로그인 처리됨

--- client/src/pages/Login/OauthLogin.js
+++ client/src/pages/Login/OauthLogin.js
@@ -5,14 +5,29 @@ import { useSearchParams } from 'react-router-dom'
 import styled from "styled-components";
 
 
-
+const REDIRECT_URI='http://localhost:3000/login/'
+///////////카카오
   const KAKAO_REST_API_KEY='57b175e9a7e058d7b81488512a16d03f'
-  const KAKAO_REDIRECT_URI='http://localhost:3000/login/'
-  const kakaoLogin=`https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+  const kakaoLogin=`https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   const kakaoOauthUrl='https://kauth.kakao.com/oauth/token'
+////////////////////
+
+///////구글
+
+const ClIENT_ID='719936769714-34a3ukvnh8suuphr41e8105ls08hmtei.apps.googleusercontent.com'
+const ClIENT_PWD_KEY= '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'
+const GoogleLogin=`https://accounts.google.com/o/oauth2/v2/auth?
+client_id=${ClIENT_ID}
+&redirect_uri=${REDIRECT_URI}
+&response_type=code
+&scope=email%20profile%20openid
+&access_type=offline`
+const googleOauthUrl ='https://oauth2.googleapis.com/token'
 
 
-const KakaoOauth = () => {
+
+
+const OauthLogin = () => {
   const [searchQ,setSearchQ] = useSearchParams()
   const kakaoCode=searchQ.get('code')
 const dispatch=useDispatch()
@@ -21,10 +36,10 @@ const dispatch=useDispatch()
 
 
 
-  // window.location.href=kakaoLogin;
+  //카카오
   function kakaoOauth(){
   if(kakaoCode){
-       fetch(`${kakaoOauthUrl}?grant_type=authorization_code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&code=${kakaoCode} `,{
+       fetch(`${kakaoOauthUrl}?grant_type=authorization_code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${kakaoCode} `,{
         method: 'POST',
         headers: {
           'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'}
@@ -35,7 +50,8 @@ const dispatch=useDispatch()
         if(data.access_token){
           fetch( "https://kapi.kakao.com/v2/user/me",{
             headers: { 'Authorization': `Bearer ${data.access_token}`,}
-          }).then((res) => res.json())
+          })
+          .then((res) => res.json())
           .then((data2)=>{
             console.log('토큰정보불러오기',data2)
             if(data2){
@@ -51,8 +67,45 @@ const dispatch=useDispatch()
 }
 
 
+
+//////////구글
+const getGoogleAccess=()=>{
+  console.log('구글오우뜨',kakaoCode)
+  if(kakaoCode){
+    fetch(`${googleOauthUrl}?code=${kakaoCode}&client_id=${ClIENT_ID}&client_secret=${ClIENT_PWD_KEY}&redirect_uri=${REDIRECT_URI}&grant_type=authorization_code`,{
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded'},
+     
+    })
+    .then((res) => res.json())
+    .then((data)=>{
+      console.log('구글엑세스',data)
+      if(data.access_token){
+        fetch(`https://www.googleapis.com/oauth2/v2/userinfo?access_token=${data.access_token}`,{
+          headers: { 'Authorization': `Bearer ${data.access_token}`,}
+        })
+        .then((res) => res.json())
+        .then((data2)=>{
+          console.log('구글토큰정보불러오기',data2)
+
+        })
+      }
+
+    })
+
+  }
+  
+  }
+
+
+
+
+
+
 useEffect(()=>{
-  kakaoOauth()
+  kakaoOauth();
+  getGoogleAccess()
 },[kakaoCode])
 
 
@@ -63,16 +116,25 @@ useEffect(()=>{
 
 
   return (
+    <>
     <LinkStyle href ={kakaoLogin} >
       <KakaoLoginButton >
         <Logo src='https://cdn-icons-png.flaticon.com/512/3991/3991999.png' alt='카카오로고'></Logo><div>카카오로 로그인하기</div>
       </KakaoLoginButton>
     </LinkStyle>
-    
+
+
+   <LinkStyle href={GoogleLogin}>
+
+    <KakaoLoginButton >
+        <Logo src='https://cdn-icons-png.flaticon.com/512/2702/2702602.png' alt='구글로고'></Logo><div>구글로 로그인하기</div>
+      </KakaoLoginButton>
+    </LinkStyle> 
+    </>
   )
 }
 
-export default KakaoOauth
+export default OauthLogin
 const LoginButton=styled.button`
   font-size: 18px;
   font-weight: 700;
